6945214b96de4295f34b35d0fc7f5d478bbb7038
etibonuni <etienne.bonanno.97@um.edu.mt>
2018-04-30 21:22:41 +0200

    progress

--- Eurovision/eurosim.R
+++ Eurovision/eurosim.R
@@ -60,7 +60,7 @@ simulate.voting <- function(voting.history, start.year, end.year, iters=10000){
   voting.countries <- sapply(X=seq(start.year,end.year), simplify=TRUE, FUN=function(year) {    
     year.votes <- voting.history %>% 
     filter(Year==year)
-    voted.countries <- unique(year.votes$To.country)
+    voted.countries <- unique(year.votes$From.country)
   
     S<-length(voted.countries)
     
@@ -80,18 +80,72 @@ simulate.voting <- function(voting.history, start.year, end.year, iters=10000){
         one_simulation=vector()
         for (year in start.year:end.year) {
           S<-voting.countries[year-start.year+1]
-          p.0 <- (S-11)/(S-1)
-          p.x <- 1/(S-1)
-          p <- c(p.0, rep(p.x, 10))
+          position <- ceiling(runif(1, 1, S))
+          vote <- ifelse(position <= 10, possible.votes[position], 0)
+          one_simulation <- c(one_simulation, vote)
+#          p.0 <- (S-11)/(S-1)
+#          p.x <- 1/(S-1)
+#          p <- c(p.0, rep(p.x, 10))
           
-          one_simulation<-c(one_simulation, sample(c(0,1,2,3,4,5,6,7,8,10,12), 1, prob=p))
+#          one_simulation<-c(one_simulation, sample(c(0,1,2,3,4,5,6,7,8,10,12), 1, prob=p))
         }
         average_simulation<-c(average_simulation, mean(one_simulation))
       }
-      simulated.votes.thresh[from.country,to.country] <- quantile(average_simulation, probs=c(0.95), names=FALSE)[1]
+      simulated.votes.thresh[from.country,to.country] <- quantile(average_simulation, probs=c(0.99), names=FALSE)[1]
     }
   }
   return(simulated.votes.thresh)
 }  
 
-simvotes <- simulate.voting(voting.history, 1975, 1980, 10)
+get.average.votes <- function(voting.history, start.year, end.year){
+  donor.countries <- unique(voting.history$From.country)
+  recip.countries <- unique(voting.history$To.country)
+  
+  average.votes <- data.frame(matrix(data=0, nrow=length(donor.countries), ncol=length(recip.countries)))
+  row.names(average.votes) <- donor.countries
+  colnames(average.votes) <- recip.countries
+
+  year.votes <- voting.history %>% 
+    filter(between(Year, start.year, end.year), Duplicate!="x") %>%
+    select(From.country, To.country, Points) %>%
+    group_by(From.country, To.country) %>% summarise_all(mean)
+  
+  for (r in 1:nrow(year.votes)){
+    fromCountry <- as.character(year.votes$From.country[r])
+    toCountry <- as.character(year.votes$To.country[r])
+    votes <- as.numeric(year.votes$Points[r])
+    average.votes[fromCountry, toCountry] <- votes
+  }
+  
+  return(average.votes)
+}
+
+find.mutual.voters <- function(average.votes, statistically.significant){
+  donor.countries = row.names(average.votes)
+  recip.countries = colnames(average.votes)
+  
+  mutual.voters <- data.frame(country.1=character(), country.2=character(), average.vote.fwd=double(), average.vote.bwd=double())
+  
+  for (donor in donor.countries){
+    for (recip in recip.countries) {
+      if (statistically.significant[donor, recip]){
+        if ( (recip %in% donor.countries) && (donor %in% recip.countries)) {
+          if (statistically.significant[recip, donor]) {
+            mutual.pair <- data.frame(country.1=donor, country.2=recip, average.vote.fwd=average.votes[donor,recip], average.vote.bwd=average.votes[recip,donor])
+            mutual.voters <- rbind(mutual.voters, mutual.pair)
+          }
+        }
+      }
+    }
+  }
+  
+  return(mutual.voters)
+}
+
+average.votes <- get.average.votes(voting.history, 1975, 1980)
+
+vote.thresholds <- simulate.voting(voting.history, 1975, 1980, 1000)
+
+statistically.significant <- average.votes>vote.thresholds
+
+mutual.voters <- find.mutual.voters(average.votes, statistically.significant)
